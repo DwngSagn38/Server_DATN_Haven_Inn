0ffb34370f43ef1f6b2421e309493783e583ae6d
DwngSagn38 <shallot38hk@gmail.com>
2024-11-18 23:21:29 +0700

    quan ly dich vu

---
+++ routes/dichvu_web.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const dichVuController = require('../controllers/dichvu_web_controller');
+const { upload } = require('../config/common/uploads');
+
+router.get('/', dichVuController.getListorByID);
+router.post('/post', upload.single('hinhAnh'), dichVuController.addDichVu);
+router.put('/put/:id', upload.single('hinhAnh'), dichVuController.suaDichVu);
+router.delete('/delete/:id', dichVuController.xoaDichVu);
+
+
+module.exports = router;
